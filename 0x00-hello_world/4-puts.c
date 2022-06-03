02995863fab3dafdcd70024023f1b8a194ad9a72
Joshua Adeoye <theadeoyejoshua@gmail.com>
2022-06-02 22:59:56 -0700

    File updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+$include <stdio.h>
 
 /**
- * main - prints a text
+ * main -prints a text
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
