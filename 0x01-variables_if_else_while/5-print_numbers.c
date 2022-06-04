b6e351d96d1d05ac6bbd830ad8302d8ea10ac018
Joshua Adeoye <theadeoyejoshua@gmail.com>
2022-06-03 18:31:56 -0700

    File added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -Prints thew numbers since 0 to 9
+ *
+ * Returns: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
