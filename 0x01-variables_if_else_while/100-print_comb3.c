ad6c5234374637cbd06bf1d026ac8554e131d74e
Joshua Adeoye <theadeoyejoshua@gmail.com>
2022-06-03 22:05:17 -0700

    File added

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,21 +11,23 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (c < i)
+		for (i = '0'; i <= '9'; i++)
 		{
-			putchar(c);
-			putchar(i);
-
-			if (c != '8' || (c == '8' && i != '9'))
+			if (c < i)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-}
 
-putchar('\n');
+	putchar('\n');
 
-return (0);
+	return (0);
 }
