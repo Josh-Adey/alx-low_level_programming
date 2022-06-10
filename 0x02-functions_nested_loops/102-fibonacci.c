cd0943ec4548c59bff8cc4359f97c5f9d6d6575d
Joshua Adeoye <theadeoyejoshua@gmail.com>
2022-06-09 18:13:35 -0700

    File added

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,17 +14,24 @@ int main(void)
 	{
 		if (i == 0)
 			printf("%ld", j);
-		else if (i == 1)printf(", %ld", k);
+		else if (i == 1)
+			printf(", %ld", k);
 		else
 		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
+			if (i == 0)
+				printf("%ld", j);
+			else if (i == 1)
+				printf(", %ld", k);
+			else
+			{
+				k += j;
+				j = k - j;
+				printf(", %ld", k);
+			}
+
+			++i;
 		}
 
-		++i;
+		printf("\n");
+		return (0);
 	}
-
-	printf("\n");
-	return (0);
-}
