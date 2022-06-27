52a31c467e12fe5d15469021d74b8746d6b0a2d0
Joshua Adeoye <robertjoshua905@gmail.com>
2022-06-27 07:28:47 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * main - Prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", char *argv[0]);
+
+
+	return (0);
+}
+
