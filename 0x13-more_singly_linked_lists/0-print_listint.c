8654eccd0f23da6cbc5daf540d1a46840fc92a48
Joshua Adeoye <robertjoshua905@gmail.com>
2022-07-20 22:47:37 -0700

    File added

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
+
 /**
- * print_listint - prints all the elements of a list_t list
+ * print_listint - prints all the elements of a list.
  * @h: head of a list.
  *
- * Return: number of elements in the list.
+ * Return: numbers of nodes.
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t nnodes = 0;
