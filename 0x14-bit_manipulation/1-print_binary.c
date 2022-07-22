07616745f780acea88d160d5888f2dc4fb66395e
Joshua Adeoye <robertjoshua905@gmail.com>
2022-07-22 06:56:03 -0700

    File added

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,14 +8,30 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
-	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-	}
-	else
+	if (n == 0)
 	{
 		_putchar('0');
+		return;
 	}
+
+	_divide(n);
+}
+
+/**
+ * _divide - ...
+ * @n: ...
+ *
+ * Return: ...
+ */
+void _divide(unsigned long int n)
+{
+	if (n < 1)
+		return;
+
+	_divide(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
 }
